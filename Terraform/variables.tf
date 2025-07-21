37410dd586271948937bb5f2f2e67715a8d65a67
Dan.Sorkin <Dsorktech@gmail.com>
2025-07-21 21:26:50 +0300

    Update variables.tf
    yes

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,3 +3,15 @@ variable "subnet_id" {
   type        = string
   default     = "subnet-0fb155b4776b96c85"
 }
+
+variable "access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
